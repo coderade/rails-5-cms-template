b673f1531e6c2e73566b5ed898863143045936ac
Valdeci Gomes <valdeci@coderade.in>
2017-03-05 01:43:58 -0300

    edit models associations, add join migrations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,8 +1,6 @@
 class Subject < ApplicationRecord
 
-  # testing One-to-one
-
-  has_one :page
+  has_many :pages
 
   scope :visible, -> {where(:visible =>true)}
   scope :invisible, -> {where(:visible =>false)}
--- db/migrate/20170223004605_rename_user_to_admin_user.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserToAdminUser < ActiveRecord::Migration[5.0]
-  def change
-    rename_table :users, :admin_users
-  end
-end
