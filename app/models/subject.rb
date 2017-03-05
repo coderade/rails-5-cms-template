2cdddaa8203d6aa105ad626773d8c9bfadb77172
Valdeci Gomes <valdeci@coderade.in>
2017-03-05 00:44:52 -0300

    testing 1:1 ActiveRecord associations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,9 @@
 class Subject < ApplicationRecord
 
+  # testing One-to-one
+
+  has_one :page
+
   scope :visible, -> {where(:visible =>true)}
   scope :invisible, -> {where(:visible =>false)}
   scope :sorted, -> {order(:position=> :asc)}
